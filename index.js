2158c44be8acfc96cee794febecf5db333fe2e5a
zone <302911425@qq.com>
2024-03-05 15:01:39 +0800

    修改

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 process.env.WX_APPID = 'wx05942a03fb56a115';
 process.env.WX_APPSECRET = '61ce1d0347c678153316a8157c8fe02a';
 
-
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
@@ -18,6 +17,9 @@ app.use(logger);
 
 let accessTokenCache = null;
 let accessTokenExpireTime = 0;
+
+let ticketCache = null;
+let ticketExpireTime = 0;
 
 // 首页
 app.get("/", async (req, res) => {
@@ -58,19 +60,31 @@ app.get("/api/wx_openid", async (req, res) => {
 
 // 获取jsapi_ticket
 app.get("/api/wx_jsapi_ticket", async (req, res) => {
-  let accessToken = accessTokenCache;
-  if (!accessToken || Date.now() >= accessTokenExpireTime) {
-    // 如果accessToken不存在或者已过期，重新获取accessToken
-    const tokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${process.env.WX_APPID}&secret=${process.env.WX_APPSECRET}`;
-    const tokenResult = await axios.get(tokenUrl);
-    accessToken = tokenResult.data.access_token;
-    accessTokenExpireTime = Date.now() + 7200 * 1000; // 设置accessToken过期时间为7200秒后
-    accessTokenCache = accessToken;
+  let ticket = ticketCache;
+  let ticketExpireTime = ticketExpireTime;
+  if (!ticket || Date.now() >= ticketExpireTime) {
+    // 如果ticket不存在或者已过期，重新获取ticket
+    let accessToken = accessTokenCache;
+    if (!accessToken || Date.now() >= accessTokenExpireTime) {
+      // 如果accessToken不存在或者已过期，重新获取accessToken
+      const tokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${process.env.WX_APPID}&secret=${process.env.WX_APPSECRET}`;
+      const tokenResult = await axios.get(tokenUrl);
+      accessToken = tokenResult.data.access_token;
+      accessTokenExpireTime = Date.now() + 7200 * 1000; // 设置accessToken过期时间为7200秒后
+      accessTokenCache = accessToken;
+    }
+
+    const ticketUrl = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${accessToken}&type=jsapi`;
+    const ticketResult = await axios.get(ticketUrl);
+    ticket = ticketResult.data.ticket;
+    ticketExpireTime = Date.now() + 7200 * 1000; // 设置ticket过期时间为7200秒后
+    ticketCache = ticket;
   }
 
-  const ticketUrl = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${accessToken}&type=jsapi`;
-  const ticketResult = await axios.get(ticketUrl);
-  res.send(ticketResult.data);
+  res.send({
+    ticket,
+    expires_in: ticketExpireTime - Date.now(), // 返回ticket的剩余过期时间
+  });
 });
 
 const port = process.env.PORT || 80;
